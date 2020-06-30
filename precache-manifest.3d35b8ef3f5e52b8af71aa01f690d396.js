45bcb244ac848cd7b22ccf6ea63a11649fa7b39a
struesdale647 <44466959+struesdale647@users.noreply.github.com>
2020-06-30 21:48:19 +0100

    Updates

---
+++ precache-manifest.3d35b8ef3f5e52b8af71aa01f690d396.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208fda0241793daa94a0bf366bd33b1d",
+    "url": "/struesdale647/robot-friends.git/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/struesdale647/robot-friends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b68f5385b44754fc65a9",
+    "url": "/struesdale647/robot-friends.git/static/css/main.855b99de.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/struesdale647/robot-friends.git/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/struesdale647/robot-friends.git/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68f5385b44754fc65a9",
+    "url": "/struesdale647/robot-friends.git/static/js/main.fa87c59f.chunk.js"
+  },
+  {
+    "revision": "74bd5f37fd9eb1284992",
+    "url": "/struesdale647/robot-friends.git/static/js/runtime-main.61f86275.js"
+  },
+  {
+    "revision": "22e05b86a5bc56cf75b9ab7683fa4309",
+    "url": "/struesdale647/robot-friends.git/static/media/Alien Robot.22e05b86.woff"
+  },
+  {
+    "revision": "296ffb102adbb763d6e455cc28391674",
+    "url": "/struesdale647/robot-friends.git/static/media/StarcraftNormal.296ffb10.woff"
+  }
+]);
